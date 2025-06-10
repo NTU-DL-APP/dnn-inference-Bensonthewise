e21b8de0c4c30fc2d135caa78135bff8df2f320b
Tatakae <127237581+Bensonthewise@users.noreply.github.com>
2025-06-10 14:33:41 +0800

    123

--- train.py
+++ train.py
@@ -4,13 +4,15 @@ import numpy as np
 from pathlib import Path
 
 # 1. 載入資料，資料預處理
+
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.fashion_mnist.load_data()
 x_train = x_train.astype(np.float32) / 255.0
 x_test  = x_test .astype(np.float32) / 255.0
 
-# 2. 定義模型
+# 2. 定義模型（改這裡）
 model = tf.keras.Sequential([
     tf.keras.layers.Flatten(input_shape=(28,28)),
+    tf.keras.layers.Dense(256, activation='relu'),
     tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.Dense(10, activation='softmax'),
 ])
@@ -19,8 +21,14 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-# 3. 訓練
-model.fit(x_train, y_train, epochs=5, batch_size=64, validation_split=0.1)
+# 3. 訓練（還有這裡）
+model.fit(
+    x_train, y_train,
+    epochs=15,            # 從 5 拉高到 15
+    batch_size=64,
+    validation_split=0.1
+)
+
 
 # 4. 評估
 loss, acc = model.evaluate(x_test, y_test, verbose=0)
